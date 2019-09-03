71531ce24a1944a40f97cafb719e14fe75fa5e54
Fredrik Palm <fredrik@thefarm.se>
2019-09-03 10:14:09 +0200

    More carthage problems

---
+++ SOCKit/SOCKit.h
@@ -0,0 +1,19 @@
+//
+//  SOCKit.h
+//  SOCKit
+//
+//  Created by Fredrik Palm on 2019-09-03.
+//  Copyright © 2019 NimbusKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOCKit.
+FOUNDATION_EXPORT double SOCKitVersionNumber;
+
+//! Project version string for SOCKit.
+FOUNDATION_EXPORT const unsigned char SOCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOCKit/PublicHeader.h>
+
+
